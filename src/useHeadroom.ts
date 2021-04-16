2accf8bebcb5d1664a7880a9384820d593ff92d4
Ilyass Ben Hakim <ilyassbenhakim2@gmail.com>
2021-04-16 10:00:55 +0000

    Adding types

---
+++ src/useHeadroom.ts
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+import UseHeadroom from '../useHeadroom';
+import { callOnPin, callOnUnpin, callOnFix, callOnUnfix } from './helpers';
+
+const useHeadroom: UseHeadroom = ({
+  onPin,
+  onUnpin,
+  fixAt = 0,
+  onFix,
+  onUnfix,
+}) => {
+  const [scroll, setScroll] = React.useState(0);
+
+  // Tracking scroll value
+  useEffect(() => {
+    const handleScroll = () => setScroll(window.scrollY);
+    window.addEventListener('scroll', handleScroll);
+    // Cleanup function
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const scrollRef = React.useRef({ scroll: scroll });
+
+  useEffect(() => callOnPin(scrollRef.current.scroll, scroll, fixAt, onPin), [
+    scroll < fixAt || scrollRef.current.scroll <= scroll,
+  ]);
+
+  // Handle onUnpin callback
+  useEffect(
+    () => callOnUnpin(scrollRef.current.scroll, scroll, fixAt, onUnpin),
+    [scroll < fixAt ? scroll < fixAt : scrollRef.current.scroll >= scroll]
+  );
+
+  // Handle onFix callback
+  useEffect(() => callOnFix(scroll, fixAt, onFix), [scroll <= fixAt]);
+
+  // Handle onUnfix callback
+  useEffect(
+    () => callOnUnfix(scrollRef.current.scroll, scroll, fixAt, onUnfix),
+    [scroll > fixAt]
+  );
+
+  // Handling the backward scroll behavior
+  useEffect(() => {
+    scrollRef.current.scroll = scroll;
+  }, [scroll]);
+
+  return scrollRef.current.scroll >= scroll || scroll <= fixAt;
+};
+export default useHeadroom;
